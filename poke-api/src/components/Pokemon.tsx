0cea5a7d5f93ddc785c9f3dce3152c7b386c7b6e
SaladinX1 <hopefulhorizon01@gmail.com>
2024-12-03 13:54:39 +0100

    ajout des informations pokemon retour jsx

--- poke-api/src/components/Pokemon.tsx
+++ poke-api/src/components/Pokemon.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import { useState } from 'react'
 import { useEffect } from 'react'
 
@@ -7,7 +7,7 @@ const BASE_URL = import.meta.env.VITE_BASE_URL_API
 
 export default function Pokemon({url}) {
 
-    const [pokeInfo, setPokeInfo] = useState(null);
+    const [pokeInfo , setPokeInfo] = useState([]);
     const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -18,9 +18,10 @@ export default function Pokemon({url}) {
         fetch(`${BASE_URL}${url}`)
         .then(data => data.json())
         .then(res => {
-          setLoading(false);
           console.log(res);
           setPokeInfo(res)
+          setLoading(false);
+
         })
         .catch(error => {
           console.log(error)
@@ -33,15 +34,21 @@ export default function Pokemon({url}) {
     } catch(error) {
       console.log(error);
     }
-  }, [])
+  }, [url])
 
-    return 
-        <>
-        {/* {!loading && pokeInfo && (
-          <div className=''>
+    return <>
 
+         {!loading && pokeInfo && (
+          <div className='flex flex-col p-2 hover:transition-all hover:scale-125 bg-green-400 m-4 cursor-pointer rounded-xl '>
+               <p><img className='h-full w-full' src={pokeInfo.sprites.front_default} alt="image"></img></p>
+              <p className='block w-auto'>{pokeInfo.name}</p>
+              <p className='block w-auto'>id : {pokeInfo.id}</p>
+              <p className='block w-auto'>Poids : {pokeInfo.weight} kg</p>
+              <p className='block w-auto'>Taille : {pokeInfo.height}</p>
+              <p className='block w-auto'>Cap. spé. : {pokeInfo.abilities[0].ability.name}</p>
           </div>
-        )} */}
+        )} 
+
         </>
   }
 
